cb73bb0071ea94fee5decad5bc2370044f22e6eb
hagikenn <146914983+hagikenn@users.noreply.github.com>
2024-10-15 10:25:37 +0900

    1-2再帰関数資料作成　完成

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include<stdio.h>
-//#include<Windows.h>
+
+int Recursive(int n) {
+	if (n <= 1) {
+		return(1);
+	}
+	return(n * Recursive(n - 1));
+}
 
 int main() {
-	/*SetConsoleOutputCP(65001);*/
-	printf("乃木坂");
+	int n = 10;//階乗する値
+	int result;//計算結果
+
+	result = Recursive(n);
+	printf("%dの階乗 = %d\n",n,result);
 
 	return 0;
 }
