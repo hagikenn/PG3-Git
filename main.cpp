ebbdfbace352f42ece6dc7e91a89be2420f4cdfe
hagikenn <146914983+hagikenn@users.noreply.github.com>
2024-10-15 13:41:13 +0900

    再帰関数の課題完了

--- main.cpp
+++ main.cpp
@@ -1,18 +1,35 @@
 #include<stdio.h>
 
-int Recursive(int n) {
-	if (n <= 1) {
-		return(1);
+int Recursive(int Rhw, int hw,int timer, int RhwResult, int hwResult) {
+	//一般の合計金額
+	hwResult = hw * timer;
+
+	//再帰の合計金額
+	RhwResult +=  Rhw;
+
+	if (hwResult <= RhwResult) {
+		printf("再帰のお金と時間  お金=%d,時給=%d,時間=%d\n", RhwResult, Rhw, timer);
+		printf("一般のお金と時間  お金=%d,時給=%d,時間=%d\n", hwResult, hw, timer);
+
+		return 0;
+	}else {
+		printf("再帰のお金と時間  お金=%d,時給=%d,時間=%d\n", RhwResult, Rhw, timer);
+		printf("一般のお金と時間  お金=%d,時給=%d,時間=%d\n\n", hwResult, hw, timer);
+		return Recursive(Rhw * 2 - 50, hw, timer + 1, RhwResult, hwResult);
 	}
-	return(n * Recursive(n - 1));
+	
 }
 
-int main() {
-	int n = 10;//階乗する値
-	int result;//計算結果
 
-	result = Recursive(n);
-	printf("%dの階乗 = %d\n",n,result);
+int main() {
+	int Rhw = 100;//再帰 時給
+	int hw = 1072;//一般的 hourly wage(時給)
+	int timer = 1;//時間（比べるのに使う）
+
+	int RhwResult = 0;//再帰の計算結果
+	int hwResult = 0;//一般の計算結果
+
+	Recursive(Rhw,hw,timer, RhwResult, hwResult);
 
 	return 0;
 }
